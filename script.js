21ba5ab1b81a46a1969bc6eb69c865a01a4f69df
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2022-09-20 12:26:16 +0200

    JS roll change player

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-//******BRUITAGE 
+// VARIABLES AND CONDITIONS
+const scoreP1 = document.querySelector("#global-p1");
+const scoreP2 = document.querySelector("#global-p2");
+const spinner1 = document.querySelector("#spin1");
+const spinner2 = document.querySelector("#spin2");
+const currentP1 = document.getElementById("round-p1");
+const currentP2 = document.getElementById("round-p2");
+const hold = document.querySelector("#hold");
+const newGame = document.querySelector("#new-game");
+const scores = [0,0];
+let currentScore = 0;
+let activePlayer = 1;
 
-function play() {
-    var audio = document.getElementById("audio");
-    audio.playbackRate = 1.7;
-    audio.play();
-  }
-  
 //**********ROLL DICE
 
 document.querySelector("#roll-dice").addEventListener("click", roll);
 
 //RANDOM NUMBER
+
 function randomNumber() {
   return Math.ceil(Math.random()*6);
 }
@@ -18,13 +24,29 @@ function randomNumber() {
 function roll() {
   const randomDice = randomNumber();
   console.log(randomDice);
-  
   // CHANGE IMG DICE
   let imgDice = document.querySelector("#img-1");
   imgDice.src = "images/dice-" + randomDice + ".png";
-}
-  
 
+  if(randomDice !==1){
+    // ADD CURRENT SCORE
+    currentScore += randomDice;
+    document.getElementById(`round-p${activePlayer}`).textContent = currentScore;
+  }else {
+    // CHANGE PLAYER
+    activePlayer = activePlayer === 1 ? 2:0;
+  }
+ 
+}
+
+
+//******BRUITAGE 
+
+function play() {
+  var audio = document.getElementById("audio");
+  audio.playbackRate = 1.7;
+  audio.play();
+}
 
 
   
