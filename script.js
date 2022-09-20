3e0668897f0c665d0af68bffc8d29cc27ca8f867
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2022-09-20 10:18:46 +0200

    Script Roll+ change img

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-// BRUITAGE 
+//******BRUITAGE 
 
 function play() {
     var audio = document.getElementById("audio");
     audio.playbackRate = 1.7;
     audio.play();
   }
+  
+//**********ROLL DICE
+
+document.querySelector("#roll-dice").addEventListener("click", roll);
+
+//RANDOM NUMBER
+function randomNumber() {
+  return Math.ceil(Math.random()*6);
+}
+
+function roll() {
+  const randomDice = randomNumber();
+  console.log(randomDice);
+  
+  // CHANGE IMG DICE
+  let imgDice = document.querySelector("#img-1");
+  imgDice.src = "images/dice-" + randomDice + ".png";
+}
+  
+
+
+
   
